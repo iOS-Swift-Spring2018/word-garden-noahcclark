3aef20423a33b5897d813021f058c05d44ab779b
Noah Clark <noah@gamersensei.com>
2018-02-12 00:51:15 -0500

    Week 2 of word garden

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -10,16 +10,114 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    var wordToGuess = "SWIFT"
+    var lettersGuessed = ""
+    let maxWrongGuesses = 8
+    var wrongGuessesRemaining = 8
+    var guessCount = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
+        formatUserGuessLabel()
+    }
+    
+    func updateUIAfterGuess() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+        guessLetterButton.isEnabled = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func formatUserGuessLabel() {
+        var revealedWord = ""
+        lettersGuessed += guessedLetterField.text!
+        
+        for letter in wordToGuess {
+            if lettersGuessed.contains(letter) {
+                revealedWord = revealedWord + " \(letter)"
+            } else {
+                revealedWord += " _"
+            }
+        }
+        revealedWord.removeFirst()
+        userGuessLabel.text = revealedWord
     }
-
-
+    
+    func guessALetter() {
+        formatUserGuessLabel()
+        guessCount += 1
+        
+        //decrements wrongGuessesRemaining and shows correct flower
+        let currentLetterGuessed = guessedLetterField.text!
+        if !wordToGuess.contains(currentLetterGuessed) {
+            wrongGuessesRemaining -= 1
+            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
+        }
+        
+        let revealedWord = userGuessLabel.text!
+        //stops game if wrongGuessesRemaining = 0
+        if wrongGuessesRemaining == 0{
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            guessCountLabel.text = "You Lost :("
+        } else if !revealedWord.contains("_") {
+            //won game
+            playAgainButton.isHidden = false
+            guessedLetterField.isEnabled = false
+            guessLetterButton.isEnabled = false
+            guessCountLabel.text = "Congrats! You Won! It took you \(guessCount) guesses."
+        } else {
+            //update guess count label
+            let guess = (guessCount == 1 ? "guess" : "guesses")
+            
+            guessCountLabel.text = "You've made \(guessCount) \(guess)"
+        }
+    }
+    
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            guessLetterButton.isEnabled = false
+        }
+        
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessALetter()
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        guessALetter()
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        guessedLetterField.resignFirstResponder()
+        playAgainButton.isHidden = true
+        guessedLetterField.isEnabled = true
+        guessLetterButton.isEnabled = false
+        flowerImageView.image = UIImage(named: "flower\(maxWrongGuesses)")
+        wrongGuessesRemaining = maxWrongGuesses
+        lettersGuessed = ""
+        formatUserGuessLabel()
+        guessCountLabel.text = "You've made 0 guesses"
+        guessCount = 0
+    }
+    
 }
 
